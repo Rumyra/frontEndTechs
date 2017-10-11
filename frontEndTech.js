2a88af22e042c59c31936f62a4822e062c923e10
Ruth John <rumyra@gmail.com>
2017-10-11 17:36:10 +0100

    FILE 🕶

---
+++ frontEndTech.js
@@ -0,0 +1,48 @@
+frontEndTech = {
+  // so someone showed me this https://coggle.it/diagram/Vz9LvW8byvN0I38x
+  // I was displeased, I think a huge amount of front end has been missed... so I'm going to make the front end bit
+
+  tools: {
+
+    textEditors: ['atom','sublime text','vs code']
+
+    terminal: [],
+
+    versionControl: ['git','svn'],
+
+
+
+  },
+
+  accessiblity: {},
+
+  html: {},
+
+  css: {
+
+    layout: ['display', 'floats', 'no layout', 'tables', 'flexbox', 'grid', 'positioning'],
+
+    styles: ['backgrounds','borders','boxshadows','filters','blendmodes'],
+
+    interaction: ['hover','animations','transitions'],
+
+    fonts: []
+
+    draw?: ['shapes','svg'],
+
+    compatibility: ['vendor prefixes','conditional comments','@supports']
+
+  },
+
+  javascript: {},
+
+  browser: {
+
+  },
+
+  webBasics: {
+    ['ftp','dns','hosting','deployment','RESTful apis','GET POST PUT DELETE',]
+  }
+
+
+}
